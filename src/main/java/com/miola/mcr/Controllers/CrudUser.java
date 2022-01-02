47033b9a1c5555232ff2269cf4f4ad1ab94db4da
ImzagnanIlyas <imzagnan.ii@gmail.com>
2022-01-02 22:25:53 +0100

    16th commit : CrudUser : Close,Update data, and clear fields after saving & Add Validation

--- src/main/java/com/miola/mcr/Controllers/CrudUser.java
+++ src/main/java/com/miola/mcr/Controllers/CrudUser.java
@@ -2,17 +2,26 @@ package com.miola.mcr.Controllers;
 
 import com.miola.mcr.Entities.User;
 import com.miola.mcr.Services.UserService;
-import com.miola.mcr.Services.UserServiceImp;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.base.AbstractMFXDialog;
 import io.github.palexdev.materialfx.controls.cell.MFXTableColumn;
 import io.github.palexdev.materialfx.controls.cell.MFXTableRowCell;
+import io.github.palexdev.materialfx.controls.enums.DialogType;
+import io.github.palexdev.materialfx.controls.factories.MFXDialogFactory;
+import javafx.animation.PauseTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 import net.rgielen.fxweaver.core.FxWeaver;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,8 +39,8 @@ import java.util.ResourceBundle;
 @FxmlView
 public class CrudUser implements Initializable {
 
-    private final ConfigurableApplicationContext applicationContext;
     private final UserService userService;
+    private final FxWeaver fxWeaver;
 
     @FXML
     private MFXTableView<User> tableView;
@@ -43,46 +52,37 @@ public class CrudUser implements Initializable {
     private MFXButton btnEdit;
 
     private Stage formWindow;
+    private Pane centerPane = null;
+    private AbstractMFXDialog dialog;
 
     @Autowired
-    public CrudUser(ConfigurableApplicationContext applicationContext, UserService userService) {
-        this.applicationContext = applicationContext;
+    public CrudUser(UserService userService, FxWeaver fxWeaver) {
+        this.fxWeaver = fxWeaver;
         this.userService = userService;
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        formWindow = new Stage();
         populateTable();
+
+        Parent root = fxWeaver.loadView(CrudUserForm.class);
+        Scene scene = new Scene(root);
+        formWindow = new Stage();
+        formWindow.initModality(Modality.APPLICATION_MODAL); // makes stage act as a modal
+        formWindow.setResizable(false); // prevents resize and removes minimize and maximize buttons
+        formWindow.initStyle(StageStyle.UNDECORATED);
+        formWindow.setScene(scene);
+        formWindow.setOnCloseRequest(ev -> {
+            this.updateTable();
+        });
+
+        dialog = MFXDialogFactory.buildDialog(DialogType.INFO, "MFXDialog - Generic Dialog", null);
+        dialog.setIsDraggable(true);
+        dialog.setPrefSize(200, 100);
+        dialog.setVisible(false);
     }
 
     private void populateTable() {
-        // dummy data to be deleted
-/*        List<User> dummyData = List.of(
-                new User(1L, "Monsieur1", "user0", "123"),
-                new User(2L, "Monsieur2", "user0", "123"),
-                new User(0L, "Monsieur0", "user0", "123"),
-                new User(3L, "Monsieur3", "user0", "123"),
-                new User(4L, "Monsieur4", "user0", "123"),
-                new User(5L, "Monsieur5", "user0", "123"),
-                new User(6L, "Monsieur6", "user0", "123"),
-                new User(7L, "Monsieur7", "user0", "123"),
-                new User(8L, "Monsieur8", "user0", "123"),
-                new User(9L, "Monsieur9", "user0", "123"),
-                new User(1L, "Monsieur1", "user0", "123"),
-                new User(2L, "Monsieur2", "user0", "123"),
-                new User(0L, "Monsieur0", "user0", "123"),
-                new User(3L, "Monsieur3", "user0", "123"),
-                new User(4L, "Monsieur4", "user0", "123"),
-                new User(5L, "Monsieur5", "user0", "123"),
-                new User(6L, "Monsieur6", "user0", "123"),
-                new User(7L, "Monsieur7", "user0", "123"),
-                new User(8L, "Monsieur8", "user0", "123"),
-                new User(9L, "Monsieur9", "user0", "123"),
-                new User(1L, "Monsieur1", "user0", "123"),
-                new User(2L, "Monsieur2", "user0", "123")
-                );*/
-
         /* load data */
         //ObservableList<User> data = FXCollections.observableArrayList(dummyData);
         ObservableList<User> users = FXCollections.observableArrayList(userService.getAllUsers());
@@ -109,14 +109,66 @@ public class CrudUser implements Initializable {
         tableView.getTableColumns().addAll(roleColumn);
     }
 
+    public  void updateTable(){
+        /* load data */
+        ObservableList<User> users = FXCollections.observableArrayList(userService.getAllUsers());
+        tableView.setItems(users);
+    }
+
+    /*
+    * int alertType :
+    *       0 -> Save Failure
+    *       1 -> Save Success
+    *       2 -> Delete Confirmation TODO Delete Confirmation
+    *       3 -> Delete Success
+    *       4 -> Validation Warning
+    * */
+    public void showAlter(int alertType){
+        if (centerPane == null) {
+            centerPane = (AnchorPane) ((BorderPane) tableView.getScene().getRoot()).getCenter();
+
+            AnchorPane.setRightAnchor(dialog, 20.0);
+            AnchorPane.setBottomAnchor(dialog, 20.0);
+            centerPane.getChildren().add(dialog);
+            dialog.setOverlayClose(true);
+        }
+        PauseTransition delay = new PauseTransition(Duration.seconds(3));
+        delay.setOnFinished( event -> dialog.close() );
+
+        switch (alertType){
+            case 0:
+                MFXDialogFactory.convertToSpecific(DialogType.ERROR, dialog);
+                dialog.setTitle("Save failed");
+                break;
+            case 1:
+                MFXDialogFactory.convertToSpecific(DialogType.INFO, dialog);
+                dialog.setTitle("Saved successfully");
+                break;
+            case 2:
+                break;
+            case 3:
+                MFXDialogFactory.convertToSpecific(DialogType.INFO, dialog);
+                dialog.setTitle("Deleted successfully");
+                break;
+            case 4:
+                MFXDialogFactory.convertToSpecific(DialogType.WARNING, dialog);
+                dialog.setTitle("Fields validation failed");
+                break;
+            default:
+        }
+        dialog.show();
+        delay.play();
+    }
+
     @FXML
     public void delete(ActionEvent event) {
-        // TODO after deleting a user the view need to be reloaded
         List<User> selectedUsers = tableView.getSelectionModel().getSelectedItems(); // gives a List<User> of selected users from tableView
         if (!selectedUsers.isEmpty()) {
             for (User user : selectedUsers){
                     userService.deleteUserById(user.getId());
             }
+            this.showAlter(3);
+            this.updateTable();
         }
     }
 
@@ -128,28 +180,27 @@ public class CrudUser implements Initializable {
             User userToBeEdit = tableView.getSelectionModel().getSelectedItems().get(0);
 
             /* show form */
-            FxWeaver fxWeaver = applicationContext.getBean(FxWeaver.class);
-            Parent root = fxWeaver.loadView(CrudUserForm.class);
-            Scene scene = new Scene(root);
-            //System.out.print(userToBeEdit.getRoleName());
             fxWeaver.getBean(CrudUserForm.class).fillData(userToBeEdit.getName(), userToBeEdit.getUsername(), userToBeEdit.getId(), userToBeEdit.getRoleName()); // send data to the form scene
             formWindow.close();
-            formWindow.setScene(scene);
+            // Specifies the owner Window (parent) for new window
+//            Stage primaryStage = (Stage) btnDelete.getScene().getWindow();
+//            formWindow.initOwner(primaryStage);
             formWindow.setTitle("Edit user");
-            formWindow.show();
+//            formWindow.show();
+            formWindow.showAndWait(); // blocks execution until the stage is closed
         }
     }
 
     @FXML
     public void add(ActionEvent event) {
         /* show form */
-        FxWeaver fxWeaver = applicationContext.getBean(FxWeaver.class);
-        Parent root = fxWeaver.loadView(CrudUserForm.class);
-        Scene scene = new Scene(root);
         formWindow.close();
-        formWindow.setScene(scene);
+        // Specifies the owner Window (parent) for new window
+//        Stage primaryStage = (Stage) btnDelete.getScene().getWindow();
+//        formWindow.initOwner(primaryStage);
         formWindow.setTitle("Add new user");
-        formWindow.show();
+//        formWindow.show();
+        formWindow.showAndWait(); // blocks execution until the stage is closed
     }
 
 }
--- src/main/java/com/miola/mcr/Controllers/CrudUserForm.java
+++ src/main/java/com/miola/mcr/Controllers/CrudUserForm.java
@@ -1,17 +1,20 @@
 package com.miola.mcr.Controllers;
 
-import com.miola.mcr.Entities.Role;
 import com.miola.mcr.Entities.User;
 import com.miola.mcr.Services.RoleService;
 import com.miola.mcr.Services.UserService;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXComboBox;
+import io.github.palexdev.materialfx.controls.MFXPasswordField;
 import io.github.palexdev.materialfx.controls.MFXTextField;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import io.github.palexdev.materialfx.utils.BindingUtils;
+import javafx.beans.property.BooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import net.rgielen.fxweaver.core.FxWeaver;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -23,6 +26,7 @@ import java.util.ResourceBundle;
 @FxmlView
 public class CrudUserForm implements Initializable {
 
+    private final FxWeaver fxWeaver;
     private final UserService userService;
     private final RoleService roleService;
 
@@ -38,9 +42,12 @@ public class CrudUserForm implements Initializable {
     @FXML
     private MFXTextField tfName;
 
-    //TODO (ILYAS) ADD PASSWORD CONFIRMATION TEXTFIELD
     @FXML
-    private MFXTextField tfPassword;
+    private MFXPasswordField pfPassword;
+
+    @FXML
+    private MFXPasswordField pfConfirmPassword;
+    private BooleanProperty pfConfirmPasswordValidator;
 
     @FXML
     private MFXTextField tfUsername;
@@ -50,23 +57,24 @@ public class CrudUserForm implements Initializable {
     private Long idUser;
 
     @Autowired
-    public CrudUserForm(UserService userService, RoleService roleService) {
+    public CrudUserForm(FxWeaver fxWeaver, UserService userService, RoleService roleService) {
+        this.fxWeaver = fxWeaver;
         this.userService = userService;
         this.roleService = roleService;
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        // TODO Fill cbRole (Role ComboBox) ILYAS -> this roleService.getAllRolesNames() returns a List<String>
-//        ObservableList<String> roles = FXCollections.observableArrayList(roleService.getAllRolesNames());
 
-        // Temporary
         cbRole.getItems().addAll(roleService.getAllRolesNames());
+
+        // Set Validation
+        this.setFieldsValidators();
     }
 
     @FXML
     void cancel(ActionEvent event) {
-
+        this.closeWindow();
     }
 
     @FXML
@@ -77,23 +85,26 @@ public class CrudUserForm implements Initializable {
         userToEditOrAdd.setPassword(getPassword());
         userToEditOrAdd.setRole(roleService.findRoleByTitle(getRole()));
 
-        if (isAnEdit){
-            userToEditOrAdd.setId(this.idUser);
-            try {
-                System.out.print(roleService.findRoleByTitle(getRole()).toString());
-                userService.editUser(userToEditOrAdd);
-                // TODO show Confirmation Msg
-            }catch (Exception e){
-                // TODO show Error Msg
+        boolean fieldsValidation = tfName.isValid() && tfUsername.isValid() && pfPassword.isValid() && pfConfirmPassword.isValid() && cbRole.isValid();
+        if (fieldsValidation){
+            if (isAnEdit){
+                userToEditOrAdd.setId(this.idUser);
+                if (userService.editUser(userToEditOrAdd)){
+                    fxWeaver.getBean(CrudUser.class).showAlter(1);
+                    this.closeWindow();
+                }else{
+                    fxWeaver.getBean(CrudUser.class).showAlter(0);
+                }
+            }else{
+                if (userService.saveUser(userToEditOrAdd)){
+                    fxWeaver.getBean(CrudUser.class).showAlter(1);
+                    this.closeWindow();
+                }else{
+                    fxWeaver.getBean(CrudUser.class).showAlter(0);
+                }
             }
         }else{
-           try {
-               System.out.print(roleService.findRoleByTitle(getRole()).toString());
-               userService.saveUser(userToEditOrAdd);
-               // TODO show Confirmation Msg
-           }catch (Exception e){
-               // TODO show Error Msg
-           }
+            fxWeaver.getBean(CrudUser.class).showAlter(4);
         }
 
     }
@@ -104,10 +115,55 @@ public class CrudUserForm implements Initializable {
         this.idUser = idUser;
         tfName.setText(name);
         tfUsername.setText(username);
-        // TODO setSelectedValue not displaying the role -> find other method
         cbRole.setSelectedValue(roleName);
         cbRole.setPromptText(roleName);
         isAnEdit = true;
+    }
+
+    public void closeWindow(){
+        this.clearFields();
+        Stage formWindow = (Stage) (tfName.getScene().getWindow());
+        formWindow.fireEvent(new WindowEvent(formWindow, WindowEvent.WINDOW_CLOSE_REQUEST));
+    }
+
+    public void setFieldsValidators(){
+        // Name TextField : Required
+        tfName.setValidated(true);
+        tfName.getValidator().add(
+                BindingUtils.toProperty(tfName.textProperty().isNotEmpty()),
+                "Required"
+        );
+
+        // Username TextField : Required
+        tfUsername.setValidated(true);
+        tfUsername.getValidator().add(
+                BindingUtils.toProperty(tfUsername.textProperty().isNotEmpty()),
+                "Required"
+        );
+
+        // Password PasswordField : Min 8 chars,
+        pfPassword.setValidated(true);
+        pfPassword.getValidator().add(
+                BindingUtils.toProperty(pfPassword.passwordProperty().length().greaterThanOrEqualTo(8)),
+                "Must be at least 8 characters long"
+        );
+
+        // Confirm Password PasswordField : match to Password,
+        pfConfirmPassword.setValidated(true);
+        pfConfirmPasswordValidator = BindingUtils.toProperty(pfConfirmPassword.passwordProperty().isEqualTo(this.getPassword()));
+            // Link PasswordField value to ConfirmPasswordField Validation
+        pfPassword.textProperty().addListener((observable, oldValue, newValue) -> {
+            pfConfirmPassword.getValidator().remove(pfConfirmPasswordValidator);
+            pfConfirmPasswordValidator = BindingUtils.toProperty(pfConfirmPassword.passwordProperty().isEqualTo(this.getPassword()));
+            pfConfirmPassword.getValidator().add(pfConfirmPasswordValidator, "Passwords do not macth");
+        });
+
+        // Role ComboBox : Required
+        cbRole.setValidated(true);
+        cbRole.getValidator().add(
+                BindingUtils.toProperty(cbRole.getSelectionModel().selectedIndexProperty().isNotEqualTo(-1)),
+                "A value must be selected"
+        );
     }
 
 
@@ -120,11 +176,20 @@ public class CrudUserForm implements Initializable {
     }
 
     public String getPassword() {
-        return tfPassword.getText();
+        return pfPassword.getPassword();
     }
 
     public String getRole() {
         return cbRole.getSelectedValue();
+    }
+
+    public void clearFields(){
+        tfName.clear();
+        tfUsername.clear();
+        pfPassword.passwordProperty().setValue("");
+        pfConfirmPassword.passwordProperty().setValue("");
+        cbRole.getSelectionModel().clearSelection();
+        cbRole.setPromptText(null);
     }
 }
 
